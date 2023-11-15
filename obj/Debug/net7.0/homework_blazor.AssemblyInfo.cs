65fbdd7cebaf5156c1d061c7d837e271a53a2157
jstojanovic7 <jsstojanovic7@gmail.com>
2023-11-15 21:59:01 +0100

    update

--- obj/Debug/net7.0/homework_blazor.AssemblyInfo.cs
+++ obj/Debug/net7.0/homework_blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homework_blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8b388da44a0b0fc7fce7752d5a773ae5ecda34")]
 [assembly: System.Reflection.AssemblyProductAttribute("homework_blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homework_blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
